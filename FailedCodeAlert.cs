ae8255fdbe89a362248c36da1b8143ba65d670df
VisEntities <visentities@gmail.com>
2025-01-30 07:23:44 +0100

    Added /codealert command to toggle code lock intrusion notifications on or off.

--- FailedCodeAlert.cs
+++ FailedCodeAlert.cs
@@ -4,19 +4,33 @@
  * Full legal terms can be found at https://game4freak.io/eula/
  */
 
+using Newtonsoft.Json;
+using Oxide.Core;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Oxide.Plugins
 {
-    [Info("Failed Code Alert", "VisEntities", "1.0.0")]
+    [Info("Failed Code Alert", "VisEntities", "1.1.0")]
     [Description("Warns players when someone tries to access their code lock with the wrong code.")]
     public class FailedCodeAlert : RustPlugin
     {
         #region Fields
 
         private static FailedCodeAlert _plugin;
+        private StoredData _storedData;
 
         #endregion Fields
+
+        #region Stored Data
+
+        public class StoredData
+        {
+            [JsonProperty("Notifications Enabled")]
+            public Dictionary<ulong, bool> NotificationsEnabled { get; set; } = new Dictionary<ulong, bool>();
+        }
+
+        #endregion Stored Data
 
         #region Oxide Hooks
 
@@ -24,6 +38,7 @@ namespace Oxide.Plugins
         {
             _plugin = this;
             PermissionUtil.RegisterPermissions();
+            _storedData = DataFileUtil.LoadOrCreate<StoredData>(DataFileUtil.GetFilePath());
         }
 
         private void Unload()
@@ -40,6 +55,13 @@ namespace Oxide.Plugins
                 return;
 
             if (codeLock.code == enteredCode)
+                return;
+
+            ulong ownerId = codeLock.OwnerID;
+            if (!_storedData.NotificationsEnabled.TryGetValue(ownerId, out bool enabled))
+                enabled = true;
+
+            if (!enabled)
                 return;
 
             NotifyCodeLockOwners(codeLock, player);
@@ -126,20 +148,119 @@ namespace Oxide.Plugins
             }
         }
 
+        public static class DataFileUtil
+        {
+            private const string FOLDER = "";
+
+            public static string GetFilePath(string filename = null)
+            {
+                if (filename == null)
+                    filename = _plugin.Name;
+
+                return Path.Combine(FOLDER, filename);
+            }
+
+            public static string[] GetAllFilePaths()
+            {
+                string[] filePaths = Interface.Oxide.DataFileSystem.GetFiles(FOLDER);
+                for (int i = 0; i < filePaths.Length; i++)
+                {
+                    filePaths[i] = filePaths[i].Substring(0, filePaths[i].Length - 5);
+                }
+
+                return filePaths;
+            }
+
+            public static bool Exists(string filePath)
+            {
+                return Interface.Oxide.DataFileSystem.ExistsDatafile(filePath);
+            }
+
+            public static T Load<T>(string filePath) where T : class, new()
+            {
+                T data = Interface.Oxide.DataFileSystem.ReadObject<T>(filePath);
+                if (data == null)
+                    data = new T();
+
+                return data;
+            }
+
+            public static T LoadIfExists<T>(string filePath) where T : class, new()
+            {
+                if (Exists(filePath))
+                    return Load<T>(filePath);
+                else
+                    return null;
+            }
+
+            public static T LoadOrCreate<T>(string filePath) where T : class, new()
+            {
+                T data = LoadIfExists<T>(filePath);
+                if (data == null)
+                    data = new T();
+
+                return data;
+            }
+
+            public static void Save<T>(string filePath, T data)
+            {
+                Interface.Oxide.DataFileSystem.WriteObject<T>(filePath, data);
+            }
+
+            public static void Delete(string filePath)
+            {
+                Interface.Oxide.DataFileSystem.DeleteDataFile(filePath);
+            }
+        }
+
         #endregion Helper Classes
+
+        #region Commands
+
+        [ChatCommand("codealert")]
+        private void cmdCodeAlert(BasePlayer player, string cmd, string[] args)
+        {
+            if (player == null)
+                return;
+
+            ulong userId = player.userID;
+            bool currentState;
+
+            if (_storedData.NotificationsEnabled.TryGetValue(userId, out bool state))
+                currentState = state;
+            else
+                currentState = true;
+
+            _storedData.NotificationsEnabled[userId] = !currentState;
+            DataFileUtil.Save(DataFileUtil.GetFilePath(), _storedData);
+
+            string messageKey;
+            if (currentState)
+                messageKey = Lang.AlertsDisabled;
+            else
+                messageKey = Lang.AlertsEnabled;
+
+            MessagePlayer(player, messageKey);
+        }
+
+        #endregion Commands
 
         #region Localization
 
         private class Lang
         {
             public const string FailedAttempt = "FailedAttempt";
+            public const string AlertsEnabled = "AlertsEnabled";
+            public const string AlertsDisabled = "AlertsDisabled";
         }
 
         protected override void LoadDefaultMessages()
         {
             lang.RegisterMessages(new Dictionary<string, string>
             {
-                [Lang.FailedAttempt] = "Someone is trying to access your code lock at {1} but they failed! Intruder: {0}"
+                [Lang.FailedAttempt] = "Someone is trying to access your code lock at {1} but they failed! Intruder: {0}",
+                [Lang.AlertsEnabled] = "Code lock intrusion alerts enabled!",
+                [Lang.AlertsDisabled] = "Code lock intrusion alerts disabled!"
             }, this, "en");
         }
 
